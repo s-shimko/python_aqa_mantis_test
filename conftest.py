2c419b1e52fffbb1a20a0bc7b6990c026e8b2025
s.shimko <sergey1shimko@gmail.com>
2018-04-04 23:29:18 +0300

    [+] pop3 added

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from fixture.application import Application
 import json
 import os.path
-
+import ftputil
 
 fixture = None
 target = None
@@ -17,16 +17,56 @@ def load_config(file):
     return target
 
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+# @pytest.fixture
+# def app(request):
+#     global fixture
+#     global target
+#     browser = request.config.getoption("--browser")
+#     web_confing = load_config(request.config.getoption("--target"))
+#     if fixture is None or not fixture.is_valid():
+#         fixture = Application(browser=browser, base_url=web_confing['web']['baseUrl'])
+#     fixture.session.ensure_login(username=web_confing['webadmin']['username'], password=web_confing['webadmin']['password'])
+#     return fixture
+
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
-    global target
     browser = request.config.getoption("--browser")
-    web_confing = load_config(request.config.getoption("--target"))
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_confing['web']['baseUrl'])
-    fixture.session.ensure_login(username=web_confing['webadmin']['username'], password=web_confing['webadmin']['password'])
+        fixture = Application(browser=browser, config=config)
+    # fixture.session.ensure_login(username=config['webadmin']['username'], password=config['webadmin']['password'])
     return fixture
+#
+#
+# @pytest.fixture(scope="session", autouse=True)
+# def configure_server(request, config):
+#     install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+#
+#     def fin():
+#         restore_server_cohfiguration(config['ftp']['host'], config['ftr']['username'], config['ftp']['password'])
+#
+#     request.addfinalizer(fin)
+#
+#
+# def install_server_configuration(host, username, password):
+#     with ftputil.FTPHost(host, username, password) as remote:
+#         if remote.path.isfile("config_inc.php.bak"):
+#             remote.remove("config_inc.php.bak")
+#         if remote.path.isfile("config_inc.php"):
+#             remote.rename("config_inc.php", "config_inc.php.bak")
+#         remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+#
+#
+# def restore_server_cohfiguration(host, username, password):
+#     with ftputil.FTPHost(host, username, password) as remote:
+#         if remote.path.isfile("config_inc.php.bak"):
+#             if remote.path.isfile("config_inc.php"):
+#                 remote.remove("config_inc.php")
+#             remote.rename("config_inc.php.bak", "config_inc.php")
 
 
 @pytest.fixture(scope="session", autouse=True)
--- fixture/application.py
+++ fixture/application.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from fixture.project import ProjectHelper
 from fixture.session import SessionHelper
+from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -16,7 +19,12 @@ class Application:
         self.wd.implicitly_wait(5)
         self.project = ProjectHelper(self)
         self.session = SessionHelper(self)
-        self.base_url = base_url
+        self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
+
+        self.config = config
+        self.base_url = config['web']['baseUrl']
 
     def is_valid(self):
         try:
